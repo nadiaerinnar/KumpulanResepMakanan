78852afd706ce7f90d5caf26386e9d6b54ac669e
Amir <amir.rhythm@gmail.com>
2020-03-11 23:04:12 +0700

    Update detail layout

--- app/src/main/java/com/example/kumpulanresepmakanan/ResepDetailActivity.kt
+++ app/src/main/java/com/example/kumpulanresepmakanan/ResepDetailActivity.kt
@@ -1,26 +1,29 @@
 package com.example.kumpulanresepmakanan
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_phone_detail.*
 
-
 class ResepDetailActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_ITEM = "extra_item"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_phone_detail)
-        var intentThatStartedThisActivity = getIntent()
 
+        if (intent.hasExtra(EXTRA_ITEM)) {
+            val makanan = intent.getParcelableExtra<ResepData>(EXTRA_ITEM)
 
-        if (intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)) {
-            var jenis = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT)
-            var nama = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TITLE)
-            var penjelasan = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEMPLATE)
-
-            tv_jenis_makanan.text = jenis
-            tv_nama_Makanan.text = nama
-            tv_penjelasan_makanan.text = penjelasan
+            makanan?.let {
+                tv_jenis_makanan.text = it.jenis
+                tv_nama_Makanan.text = it.foodName
+                tv_penjelasan_makanan.text = it.penjelasan
+                Glide.with(applicationContext).load(it.poster).into(img_poster)
+            }
         }
     }
 }
